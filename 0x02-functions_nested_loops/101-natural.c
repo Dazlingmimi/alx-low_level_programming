93feeb19de397c01ee42ed1b9cf4e252ff57d8e5
dazlingmimi <mimiphilip222@gmail.com>
2023-02-23 12:47:06 -0800

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,51 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+/**
+  * main - computes and prints the sum of all the multiples of
+  * 3 or 5 below 1024.
+  * Return: Nothing.
+  */
 
-
-	/**
-	 * print_times_table - prints the n times table, starting with 0
-	 * @n: number of the times table
-	 */
-	void print_times_table(int n)
-	{
-		int i, j, k;
-
-
-		if (n >= 0 && n <= 15)
-		{
-			for (i = 0; i <= n; i++)
-			{
-				for (j = 0; j <= n; j++)
-				{
-					k = j * i;
-					if (j == 0)
-					{
-						_putchar(k + '0');
-					} else if (k < 10 && j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					} else if (k >= 10 && k < 100)
+int main(void)
 {
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((k / 10) + '0');
-						_putchar((k % 10) + '0');
-					} else if (k >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((k / 100) + '0');
-						_putchar(((k / 10) % 10) + '0');
-						_putchar((k % 10) + '0');
-					}
-				}
-				_putchar('\n');
-			}
-		}
-	}
+	int i, res;
 
-
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+	printf("%d\n", res);
+	return (0);
+}
