102e004c687fc151c42359003d02124411ed5e05
dazlingmimi <mimiphilip222@gmail.com>
2023-03-13 15:34:01 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,45 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include main.h"
+#include <stdlib.h>
+#include "main.h"
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *str_concat(char *s1, char *s2)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	int end1, end2, i = 0;
+	char *array;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (s1 == NULL || s2 == NULL)
+		s1 = s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (end1 = 0; end1 <= *s1; end1++)
 	{
 	}
 
-	for (j = 0; s2[j] != '\0'; j++)
+	for (end2 = 0; end2 <= *s2; end2++)
 	{
 	}
 
-	if (n < j)
-		j = n;
+	array = malloc(sizeof(char) * (end1 + end2 + 1));
 
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	while (*s1)
 	{
-		arr[co] = s2[co_2];
-		co++;
+		array[i] = *s1;
+		i++;
+		s1++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+
+	while (*s2)
+	{
+		array[i] = *s2;
+		i++;
+		s2++;
+	}
+	return (array);
 }
