049942d36a3c77a59e5c872b63579781a0909fc0
Dazlingmimi <mimiphilip222@gmail.com>
2023-03-31 16:47:25 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,86 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * _strlen - function to get length of a string
+ * @s: string
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: Length of a array of characters
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: string to copy
+ *
+ * Return: Pointer
+ */
+char *_strdup(const char *str)
+{
+	int l = 0, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+		*(s + i) = *(str + i);
+	return (s);
+}
+/**
+ * add_node - adds a new node at the beginning of a singly linked list
+ * @head: pointer to head of singly linked list
+ * @str: string to add as new node in list
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	char *tmp;
+	list_t *new_node = malloc(sizeof(list_t));
 
- while (str[len])
- len++;
+	if (new_node == 0)
+		return (0);
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+	if (str == 0)
+	{
+		new_node->str = 0;
+		new_node->len = 0;
+	}
+	else
+	{
+		tmp = _strdup(str);
+		if (tmp == 0)
+		{
+			free(new_node);
+			return (0);
+		}
+		new_node->str = tmp;
+		new_node->len = _strlen(tmp);
+	}
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
-
- return (*head);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
 
